46cfa792691c2bd57178ddfb00f6a6fe942de4f8
Pete <pgm987@gmail.com>
2013-04-29 19:13:36 +0100

    Added Articles Schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,5 @@
+DROP DATABASE hackthis;
+
 CREATE DATABASE hackthis;
 USE hackthis;
 
@@ -27,5 +29,58 @@ CREATE TABLE users_profile (
 	`lastfm` varchar(16),
 	`forum_signature` text,
 	PRIMARY KEY (`user_id`),
+	FOREIGN KEY (`user_id`) REFERENCES users (`user_id`)
+) ENGINE=InnoDB;
+
+/*
+	ARTICLES
+*/
+CREATE TABLE articles_categories (
+	`category_id` int(7) NOT NULL AUTO_INCREMENT,
+	`parent_id` int(7) DEFAULT 0,
+	`title` varchar(32),
+	PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE articles (
+	`article_id` int(7) NOT NULL AUTO_INCREMENT,
+	`user_id` int(7) NOT NULL,
+	`title` varchar(32) NOT NULL,
+	`slug` varchar(32) NOT NULL,
+	`category_id` int(7) NOT NULL,
+	`body` TEXT  NOT NULL,
+	`thumbnail` varchar(16), 
+	`submitted` timestamp DEFAULT CURRENT_TIMESTAMP,
+	`updated` timestamp,
+	`featured` int(1),
+	`views` int(5),
+	PRIMARY KEY (`article_id`),
+        FOREIGN KEY (`user_id`) REFERENCES users (`user_id`),
+        FOREIGN KEY (`category_id`) REFERENCES articles_categories (`category_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE articles_draft (
+	`article_id` int(7) NOT NULL AUTO_INCREMENT,
+	`user_id` int(7) NOT NULL,
+	`title` varchar(32) NOT NULL,
+	`category_id` int(7) NOT NULL,
+	`body` TEXT NOT NULL,
+	`time` timestamp DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`article_id`),
+	FOREIGN KEY (`user_id`) REFERENCES users (`user_id`),
+	FOREIGN KEY (`category_id`) REFERENCES articles_categories (`category_id`) 
+) ENGINE=InnoDB;
+
+CREATE TABLE articles_audit (
+	`audit_id` int(7) NOT NULL AUTO_INCREMENT,
+	`article_id` int(7) NOT NULL, 
+	`draft` tinyint(1) NOT NULL,
+	`field` varchar(9) NOT NULL,
+	`old_value` TEXT NOT NULL,
+	`new_value` TEXT NOT NULL,
+	`time` timestamp DEFAULT CURRENT_TIMESTAMP,
+	`user_id` int(7) NOT NULL,
+	`comment` TEXT NULL,
+	PRIMARY KEY (`audit_id`,`article_id`,`draft`,`field`),
 	FOREIGN KEY (`user_id`) REFERENCES users (`user_id`)
 ) ENGINE=InnoDB;
