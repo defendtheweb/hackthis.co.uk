fa78e62ab606dbaa1f91740addbe4da9e0e0635a
0x6C77 <0x6C77@gmail.com>
2013-05-02 04:44:42 +0100

    Example config file

---
+++ files/example.config.php
@@ -0,0 +1,11 @@
+<?php
+	// Site variables
+	$config['path'] = '/';
+
+	// Database configuration
+	$config['db']['driver'] = 'mysql';
+	$config['db']['host'] = 'localhost';
+	$config['db']['username'] = 'root';
+	$config['db']['password'] = 'pass';
+	$config['db']['database'] = 'hackthis';
+?>
