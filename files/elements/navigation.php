f56c5b23e0c8d3c7260b3bebe35c3293fa2c65b8
0x6C77 <0x6C77@gmail.com>
2013-05-04 15:52:29 +0100

    First navigation incarnation

---
+++ files/elements/navigation.php
@@ -0,0 +1,41 @@
+        </div>
+        <div id="global_nav">
+            <nav class="container row">
+                <ul>
+                    <li><a href='/'>home</a></li>
+                    <li><a href='/'>levels</a>
+                        <ul>
+                            <li><a href='/'>Main</a>
+                                <ul>
+                                    <li><a href='/'>Main</a></li>
+                                    <li><a href='/'>Basic+</a></li>
+                                    <li><a href='/'>Javascript</a></li>
+                                </ul>
+                            </li>
+                            <li><a href='/'>Basic+</a>
+                                <ul>
+                                    <li><a href='/'>Level 1</a></li>
+                                </ul>
+                            </li>
+                            <li><a href='/'>Javascript</a></li>
+                        </ul>
+                    </li>
+                    <li><a href='/'>articles</a></li>
+                    <li><a href='/'>forum</a></li>
+                    <li><a href='/'>irc</a>
+                        <ul>
+                            <li><a href='/'>Chat Online</a></li>
+                            <li><a href='/'>stats</a></li>
+                            <li><a href='/'>about</a></li>
+                        </ul>
+                    </li>
+                    <li><a href='/'>contact us</a></li>
+
+                    <li class='right'><a href='/'><i class="icon-cog"></i></a></li>
+                    <li class='right active'><a href='/'><i class="icon-envelope-alt"></i></a></li>
+                    <li class='right'><a href='/'><i class="icon-user"></i></a></li>
+                    <li class='right'><a href='/'><i class="icon-search"></i></a></li>
+                </ul>
+            </nav>
+        </div>
+        <div id="body_wrap" class="container row">
