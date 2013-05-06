2633ce0d77dfd1cec34498796fddf30ec518939f
0x6C77 <0x6C77@gmail.com>
2013-05-06 18:59:37 +0100

    Addition of bbcode parser

--- files/class.app.php
+++ files/class.app.php
@@ -7,9 +7,19 @@
 				throw new Exception('Config error');
 
 			$this->config = $config;
+
+			$this->utils = new utils();
+
+			$this->custom_css = Array();
+			$this->custom_js = Array();
+
+			require('vendor/nbbc.php');
+			$this->bbcode = new BBCode;
+			array_push($this->custom_css, 'bbcode.css');
+			$this->bbcode->SetDetectURLs(true);
 		}
 
-		public function config($key){
+		public function config($key) {
 			return $this->config[$key];
 		}
 	}
