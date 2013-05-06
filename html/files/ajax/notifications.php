4ab6ddc2d5fe65b6cb609f86dc8c1b9a01a6b3ee
0x6C77 <0x6C77@gmail.com>
2013-05-06 23:41:58 +0100

    Notifications gather data from respective tables, needs reworking

--- html/files/ajax/notifications.php
+++ html/files/ajax/notifications.php
@@ -5,13 +5,23 @@
 
     if (isset($_GET['events'])) {
         // Get items
-        $st = $db->prepare("SELECT notification_id AS id, type, UNIX_TIMESTAMP(time) AS timestamp, seen, item_id, username
+        $st = $db->prepare("SELECT notification_id AS id, type, UNIX_TIMESTAMP(users_notifications.time) AS timestamp, seen, username, label, colour, users_friends.status
             FROM users_notifications
             LEFT JOIN users
-            ON users_notifications.from_id = users.user_id
+            ON (users_notifications.from_id = users.user_id)
+
+            LEFT JOIN users_friends
+            ON (users_friends.user_id = users.user_id AND friend_id = :user_id)
+
+            LEFT JOIN medals
+            ON (item_id = medal_id)
+            LEFT JOIN medals_colours
+            ON (medals.colour_id = medals_colours.colour_id)
+
             WHERE users_notifications.user_id = :user_id
-            ORDER BY time DESC
+            ORDER BY users_notifications.time DESC
             LIMIT 5");
+
         $st->execute(array(':user_id' => $user_id));
         $result = $st->fetchAll();
 
@@ -25,9 +35,12 @@
         $st = $db->prepare("UPDATE users_notifications
             SET seen = '1'
             WHERE user_id = :user_id");
-        $st->execute(array(':user_id' => $user_id));    
+        $st->execute(array(':user_id' => $user_id));
 
-        echo json_encode($result);
+        $result = json_encode($result);
+        // Remove null entries
+        echo preg_replace('/,\s*"[^"]+":null|"[^"]+":null,?/', '', $result);
+
     } else if (isset($_GET['pm'])) {
         // Get items
         $st = $db->prepare("SELECT pm.pm_id, title, username, message, UNIX_TIMESTAMP(MAX(time)) as timestamp, 1 as seen
