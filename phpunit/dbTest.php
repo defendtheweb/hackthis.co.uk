b2707d0e4f51a4a998bc02d496ffbf9a3406e432
0x6C77 <0x6C77@gmail.com>
2013-05-08 14:24:40 +0100

    Don't call me an idiot

--- phpunit/dbTest.php
+++ phpunit/dbTest.php
@@ -4,7 +4,7 @@ class DBTest extends PHPUnit_Framework_TestCase {
         try {
             $dsn = "mysql:host=localhost";
             $dsn .= ";dbname=hackthis";
-            $this->db = new PDO($dsn, 'root', 'meow');
+            $this->db = new PDO($dsn, 'ubuntu');
         } catch(PDOException $e) {
             die($e->getMessage());
         }
@@ -14,15 +14,15 @@ class DBTest extends PHPUnit_Framework_TestCase {
         $this->db->query("INSERT INTO users (`username`, `password`) VALUES ('flabbyrabbit', 'pass');");
         $this->db->query("INSERT INTO users (`username`, `password`) VALUES ('osaka', 'pass2');");
 
-        // $st = $this->db->query("SELECT count(user_id) AS count FROM users;");
-        // $row = $st->fetch();
-        // $this->assertEquals(2, $row['count']);
+        $st = $this->db->query("SELECT count(user_id) AS count FROM users;");
+        $row = $st->fetch();
+        $this->assertEquals(2, $row['count']);
 
-        // $st = $this->db->query("SELECT password, score, status FROM users WHERE username = 'flabbyrabbit'");
-        // $row = $st->fetch();
-        // $this->assertEquals('pass', $row['password']);
-        // $this->assertEquals(0, $row['score']);
-        // $this->assertEquals(1, $row['status']);
+        $st = $this->db->query("SELECT password, score, status FROM users WHERE username = 'flabbyrabbit'");
+        $row = $st->fetch();
+        $this->assertEquals('pass', $row['password']);
+        $this->assertEquals(0, $row['score']);
+        $this->assertEquals(1, $row['status']);
     }
 
     /**
