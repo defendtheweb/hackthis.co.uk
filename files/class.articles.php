59ac93fefd161235c5d71dd58e081bf3dcd5dcbf
0x6C77 <0x6C77@gmail.com>
2013-05-08 23:34:30 +0100

    Single and global news views

--- files/class.articles.php
+++ files/class.articles.php
@@ -22,5 +22,24 @@
 
             return $result;
         }
+
+        public function get_article($slug) {
+            global $db;
+
+            // Group by required for count
+            $st = $db->prepare('SELECT username, title, slug, body, submitted, updated, count(comments.comment_id) as comments
+                    FROM articles
+                    LEFT JOIN users
+                    ON users.user_id = articles.user_id
+                    LEFT JOIN articles_comments as comments
+                    ON articles.article_id = comments.article_id
+                    WHERE articles.slug = :slug
+                    GROUP BY articles.article_id
+                    ORDER BY submitted DESC');
+            $st->execute(array(':slug' => $slug));
+            $result = $st->fetch();
+
+            return $result;
+        }
     }
 ?>
--- html/news.php
+++ html/news.php
@@ -2,14 +2,25 @@
     require_once('header.php');
 
     $articles = new articles();
-    $news_articles = $articles->get_articles(0);
+
+    if (isset($_GET['slug'])) {
+        $news_article = $articles->get_article($_GET['slug']);
+
+        $news_articles = array($news_article);
+    } else {
+        $news_articles = $articles->get_articles(0);
+    }
 ?>
                     <section class='news'>
 <?php
-    foreach ($news_articles as $article) {
+    foreach ($news_articles as $article):
 ?>
                         <article>
                             <header class='title'>
+                                <?php if ($user->news_priv): ?>
+                                    <a href='/news/<?=$article->slug;?>?delete' class='button right confirmation' data-confirm='Delete news post'><i class='icon-trash'></i></a>
+                                    <a href='/news/<?=$article->slug;?>?edit' class='button right'><i class='icon-pencil'></i></a>
+                                <?php endif; ?>
                                 <h1><a href='/news/<?=$article->slug;?>'><?=$article->title;?></a></h1>
                                 <time pubdate datetime="<?=date('c', strtotime($article->submitted));?>"><?=date('d/m/Y', strtotime($article->submitted));?></time> - <?=$app->utils->username_link($article->username);?>
                                 <a href='/news/<?=$article->slug;?>#comments' class='right'><?=$article->comments;?> comment<?=($article->comments == 1)?'':'s';?></a>
@@ -19,7 +30,7 @@
                             ?>
                         </article>
 <?php
-    }
+    endforeach;
 ?>
                     </section>
 
