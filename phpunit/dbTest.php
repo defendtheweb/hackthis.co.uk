4213f13c788bdacc098e5e12a016a2dfba8b64dd
0x6C77 <0x6C77@gmail.com>
2013-05-08 13:52:34 +0100

    .

--- phpunit/dbTest.php
+++ phpunit/dbTest.php
@@ -17,14 +17,17 @@ class DBTest extends PHPUnit_Framework_TestCase {
 
         $st = $db->prepare("SELECT count(user_id) AS count FROM users");
         $row = $st->fetch();
-        print_r($row);
-        $this->assertEquals(2, $row['count']);
+        if ($row) {
+            $this->assertEquals(2, $row['count']);
+        }
 
         $st = $db->prepare("SELECT password, score, status FROM users WHERE username = 'flabbyrabbit'");
         $row = $st->fetch();
-        $this->assertEquals('pass', $row['password']);
-        $this->assertEquals(0, $row['score']);
-        $this->assertEquals(1, $row['status']);
+        if ($row) {
+            $this->assertEquals('pass', $row['password']);
+            $this->assertEquals(0, $row['score']);
+            $this->assertEquals(1, $row['status']);
+        }
     }
 }
 ?>
