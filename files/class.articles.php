fd9df7e9dbbaf950abd85b6a8be14dccc82841ff
0x6C77 <0x6C77@gmail.com>
2013-05-09 17:33:18 +0100

    Added msg styles form error, warning and info

--- files/class.articles.php
+++ files/class.articles.php
@@ -8,14 +8,14 @@
             global $db;
 
             // Group by required for count
-            $st = $db->prepare('SELECT username, title, slug, body, submitted, updated, count(comments.comment_id) as comments
-                    FROM articles
+            $st = $db->prepare('SELECT a.article_id AS id, username, title, slug, body, submitted, updated, count(comments.comment_id) as comments
+                    FROM articles a
                     LEFT JOIN users
-                    ON users.user_id = articles.user_id
+                    ON users.user_id = a.user_id
                     LEFT JOIN articles_comments as comments
-                    ON articles.article_id = comments.article_id
-                    WHERE articles.category_id = :cat_id
-                    GROUP BY articles.article_id
+                    ON a.article_id = comments.article_id
+                    WHERE a.category_id = :cat_id
+                    GROUP BY a.article_id
                     ORDER BY submitted DESC');
             $st->execute(array(':cat_id' => $category_id));
             $result = $st->fetchAll();
@@ -27,14 +27,14 @@
             global $db;
 
             // Group by required for count
-            $st = $db->prepare('SELECT username, title, slug, body, submitted, updated, count(comments.comment_id) as comments
-                    FROM articles
+            $st = $db->prepare('SELECT a.article_id AS id, username, title, slug, body, submitted, updated, count(comments.comment_id) as comments
+                    FROM articles a
                     LEFT JOIN users
-                    ON users.user_id = articles.user_id
+                    ON users.user_id = a.user_id
                     LEFT JOIN articles_comments as comments
-                    ON articles.article_id = comments.article_id
-                    WHERE articles.slug = :slug
-                    GROUP BY articles.article_id
+                    ON a.article_id = comments.article_id
+                    WHERE a.slug = :slug
+                    GROUP BY a.article_id
                     ORDER BY submitted DESC');
             $st->execute(array(':slug' => $slug));
             $result = $st->fetch();
