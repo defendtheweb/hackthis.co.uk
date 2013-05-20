fa93fab093ba2f536d4c8671a232bc9e8bc1b012
0x6C77 <0x6C77@gmail.com>
2013-05-20 01:19:19 +0100

    Reenabled happyjs

--- html/files/js/comments.js
+++ html/files/js/comments.js
@@ -1,12 +1,26 @@
 $(function() {
     var item_id = $("#comments").attr("data-id");
-    var commentsTmpl =  '<tmpl><article id="comment_${comment_id}">'+
+    var commentsTmpl =  '<tmpl><article data-id="${id}">'+
                         '    <header>'+
                         '        <div class="right">'+
-                        '            <time pubdate datetime="${time}"></time>'+
-                        '            {{if owner}}<a href="#" class="clean comment_delete">Delete</a>{{/if}}'+
+                        '            <time pubdate datetime="${time}">${timeSince(time)}</time>'+
+                        '            <div class="more"><i class="icon-menu"></i>'+
+                        '                <ul>'+
+                        '                    {{if owner}}<li><a href="#">Edit</a></li>{{/if}}'+
+                        '                    {{if owner}}<li class="seperator"><a href="#">Delete</a></li>{{/if}}'+
+                        '                    <li><a href="#" class="comment-reply">Reply</a></li>'+
+                        '                    <li><a href="#">PM User</a></li>'+
+                        '                    <li><a href="#" class="comment-report">Report</a></li>'+
+                        '                </ul>'+
+                        '            </div>'+
                         '        </div>'+
-                        '        <a href=\'/user/${username}\' class="user clean">${username}</a>'+
+                        '        <span class="strong">'+
+                        '            {{if owner}}'+
+                        '                <img src="http://www.hackthis.co.uk/users/images/28/1:1/${image}.jpg"/> You'+
+                        '            {{else}}'+
+                        '                <a href=\'/user/${username}\'><img src="http://www.hackthis.co.uk/users/images/28/1:1/${image}.jpg"/> ${username}</a>'+
+                        '            {{/if}}'+
+                        '        </span>'+
                         '    </header>'+
                         '    <div class="body">'+
                         '        {{html comment}}'+
@@ -61,9 +75,6 @@ $(function() {
     function renderComment(json, submit) {
         if (json.status == true) {
             var comments = json.comments;
-            // $.each(comments, function(index, item) {
-            //     item.pretty_date = prettyDate(item.time);
-            // });
             
             if (submit) {
                 $(commentsTmpl).tmpl(comments).hide().prependTo("#comments_container").slideDown();
@@ -75,4 +86,72 @@ $(function() {
             }
         }
     }
+
+    // Menu handlers
+    var mainEditor = $('.wysiwyg')[0];
+    $('#comments').on('click', '.comment-reply', function(e) {
+        e.preventDefault();
+
+        var parent = $(this).closest('header').siblings(".body");
+
+        // Check for existing editor
+        var tmp = parent.children('form');
+        if (tmp.length) {
+            newEditor = tmp;
+        } else {
+            var newEditor = $(mainEditor).clone();
+            var submitButton = $('<input>', {class: 'submit button right', type: 'submit', value: 'Submit'});
+            var cancelButton = $('<a>', {class: 'cancel right', href: '#', text: 'Cancel'});
+            var $form = $("<form>").append(newEditor).append(submitButton).append(cancelButton);
+
+            if (parent.find('article').length)
+                $(parent.find('article')[0]).before($form);
+            else
+                parent.append($form);
+        }
+        
+        newEditor.children('textarea').focus();
+
+        var wheight = $(window).height();
+        var pos = newEditor.offset().top - (wheight / 2) + newEditor.height();
+        $('body, html').animate({ scrollTop: pos }, 400);
+    }).on('click', '.cancel', function(e) {
+        e.preventDefault();
+        var parent = $(this).closest('form').slideUp(200, function() { $(this).remove() });
+    }).on('click', '.submit', function(e) {
+        e.preventDefault();
+
+        var $article = $(this).closest('article');
+
+        var parent_id = $article.attr("data-id");
+        if (!parent_id)
+            parent_id = 0;
+
+        var $parent = $(this).parent();
+        var body = $parent.find('textarea').val();
+
+        $parent.children('.msg').remove();
+
+        $.post('/files/ajax/comments.php?action=add', {"id": item_id, "parent": parent_id, "body": body},
+            function(data) {
+                if (data.status) {
+                    var msg = $('<div>', {class: 'hide msg msg-good'}).append($('<i>', {class: 'icon-good'})).append("Comment submitted");
+                    var newComment = $.tmpl("commentsTmpl", data.comment);
+
+                    if (parent_id === 0) {
+                        msg.hide().prependTo($parent).slideDown();
+                        $parent.find('textarea').val('');
+                        $parent = $('#comments_container');
+                        newComment.hide().prependTo($parent).slideDown();
+                    } else {
+                        msg.prependTo(newComment);
+                        $parent.after(newComment);
+                        $parent.remove();
+                    }
+                } else {
+                    alert("error or something");
+                }
+            },
+            'json');
+    });
 });
--- html/files/js/guest.js
+++ html/files/js/guest.js
@@ -1,59 +1,59 @@
-// $(function() {
-//     var tests = {
-//         user: function (val) {
-//             return val.length > 3 &&
-//                    val.length <= 16 &&
-//                    /^[0-9A-Za-z_-][0-9A-Za-z_.-]*[0-9A-Za-z_-]$/.test(val);
-//         },
+$(function() {
+    var tests = {
+        user: function (val) {
+            return val.length > 3 &&
+                   val.length <= 16 &&
+                   /^[0-9A-Za-z_-][0-9A-Za-z_.-]*[0-9A-Za-z_-]$/.test(val);
+        },
 
-//         email: function (val) {
-//             return /^(?:\w+\.?)*\w+@(?:\w+\.)+\w+$/.test(val);
-//         },
+        email: function (val) {
+            return /^(?:\w+\.?)*\w+@(?:\w+\.)+\w+$/.test(val);
+        },
           
-//         minLength: function (val, length) {
-//             return val.length >= length;
-//         },
+        minLength: function (val, length) {
+            return val.length >= length;
+        },
           
-//         maxLength: function (val, length) {
-//             return val.length <= length;
-//         },
+        maxLength: function (val, length) {
+            return val.length <= length;
+        },
           
-//         equal: function (val1, val2) {
-//             return (val1 == val2);
-//         }
-//     };
+        equal: function (val1, val2) {
+            return (val1 == val2);
+        }
+    };
 
-//     $('#login_form').isHappy({
-//         fields: {
-//             '#username': {
-//                 required: true
-//             },
-//             '#password': {
-//                 required: true
-//             }
-//         }
-//     });
+    $('#login_form').isHappy({
+        fields: {
+            '#username': {
+                required: true
+            },
+            '#password': {
+                required: true
+            }
+        }
+    });
 
-//     $('#registration_form').isHappy({
-//         fields: {
-//             '#reg_username': {
-//                 required: true,
-//                 test: tests.user
-//             },
-//             '#reg_password': {
-//                 required: true
-//             },
-//             '#reg_password_2': {
-//                 required: true,
-//                 test: tests.equal,
-//                 arg: function () {
-//                     return $('#reg_password').val();
-//                 }
-//             },
-//             '#reg_email': {
-//                 required: true,
-//                 test: tests.email
-//             }
-//         }
-//     });
-// });
+    $('#registration_form').isHappy({
+        fields: {
+            '#reg_username': {
+                required: true,
+                test: tests.user
+            },
+            '#reg_password': {
+                required: true
+            },
+            '#reg_password_2': {
+                required: true,
+                test: tests.equal,
+                arg: function () {
+                    return $('#reg_password').val();
+                }
+            },
+            '#reg_email': {
+                required: true,
+                test: tests.email
+            }
+        }
+    });
+});
