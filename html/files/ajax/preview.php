0159c3fb6ef48450f7ad91940af1160295726018
0x6C77 <0x6C77@gmail.com>
2013-06-03 18:22:25 +0100

    Fixed editor preview to use new parse function

--- html/files/ajax/preview.php
+++ html/files/ajax/preview.php
@@ -2,5 +2,5 @@
     (!isset($_POST['data'])) && die('Error processing response');
 
     require_once('init.php');
-    echo $app->bbcode->Parse($_POST['data']);
+    echo $app->parse($_POST['data']);
 ?>
