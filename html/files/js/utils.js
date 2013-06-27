1f6b8181b7ae2e579a201a30d841e002228656f5
0x6C77 <0x6C77@gmail.com>
2013-06-27 17:41:16 +0100

    Tweaked timeSince JS function so 'ago' is only prepended when not short

--- html/files/js/utils.js
+++ html/files/js/utils.js
@@ -24,7 +24,7 @@ function timeSince(oldD, short) {
     diff = Math.round((newD.getTime() - oldD.getTime()) / 1000);
     
     if (!diff)
-        return 'secs ago';
+        return "secs" + (!short ? " ago" : '');
 
     isSameDay = (oldD.getDate() == newD.getDate() 
                  && oldD.getMonth() == newD.getMonth()
