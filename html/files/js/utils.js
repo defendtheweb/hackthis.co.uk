e67f970ff768135413b11ac5ae98de54b5cb88bc
0x6C77 <0x6C77@gmail.com>
2013-07-01 23:11:46 +0100

    Added createCookie JS function to work with WYSIWYG send options

--- html/files/js/utils.js
+++ html/files/js/utils.js
@@ -110,4 +110,13 @@ function PopupCenter(url, title, w, h) {
     if (window.focus) {  
         newWindow.focus();  
     }  
-}  
+}
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else var expires = "";
+    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
+}
