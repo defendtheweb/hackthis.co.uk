63ac27b0563ab5afd67a1dc6520ecacfc05534f7
0x6C77 <0x6C77@gmail.com>
2013-07-05 17:45:26 +0100

    Disables ability to get non-news items, also shows correct error message when user navigates to an invalud uri

--- html/news.php
+++ html/news.php
@@ -9,24 +9,32 @@
     $single = false;
     if (isset($_GET['slug'])) {
         $single = true;
-        $news_article = $articles->get_article($_GET['slug']);
-
-        $news_articles = array($news_article);
+        $news_article = $articles->getArticle($_GET['slug'], 0);
+        if ($news_article)
+            $news_articles = array($news_article);
     } else {
-        $news_articles = $articles->get_articles(0);
+        $news_articles = $articles->getArticles(0);
     }
 ?>
                     <section class='news'>
 <?php
-    foreach ($news_articles as $article):
-        $article->title = $app->parse($article->title, false);
-        $article->body = $app->parse($article->body);
+    if (!isset($news_articles) || !count($news_articles)):
+?>
+                        <div class='msg msg-error'>
+                            <i class='icon-error'></i>
+                            Article not found
+                        </div>
+<?php
+    else:
+        foreach ($news_articles as $article):
+            $article->title = $app->parse($article->title, false);
+            $article->body = $app->parse($article->body);
 ?>
                         <article>
                             <header class='title clearfix'>
                                 <?php if ($user->admin_pub_priv): ?>
-                                    <a href='/admin/news.php?action=delete&slug=<?=$article->slug;?>' class='button right'><i class='icon-trash'></i></a>
-                                    <a href='/admin/news.php?action=edit&slug=<?=$article->slug;?>' class='button right'><i class='icon-pencil'></i></a>
+                                    <a href='/admin/articles.php?action=delete&slug=<?=$article->slug;?>' class='button right'><i class='icon-trash'></i></a>
+                                    <a href='/admin/articles.php?action=edit&slug=<?=$article->slug;?>' class='button right'><i class='icon-pencil'></i></a>
                                 <?php endif; ?>
                                 <h1><a href='/news/<?=$article->slug;?>'><?=$article->title;?></a></h1>
                                 <time pubdate datetime="<?=date('c', strtotime($article->submitted));?>"><?=$app->utils->timeSince($article->submitted);?></time>
@@ -50,13 +58,14 @@
                             ?>
                         </article>
 <?php
-        if ($single) {
-            $comments = array("id"=>$article->id,"title"=>$article->title,"count"=>$article->comments);
-            include('elements/comments.php');
-        }
+            if ($single) {
+                $comments = array("id"=>$article->id,"title"=>$article->title,"count"=>$article->comments);
+                include('elements/comments.php');
+            }
 
 
-    endforeach;
+        endforeach;
+    endif;
 ?>
                     </section>
 
