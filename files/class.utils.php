f06a7e16c4610118a8c96d01a2e56171928851ad
0x6C77 <0x6C77@gmail.com>
2013-07-11 18:33:40 +0100

    Function to cut text while keeping html in tact

--- files/class.utils.php
+++ files/class.utils.php
@@ -17,18 +17,19 @@
 
         public function check_user($str) {
             if (strlen($str) <= 16 && strlen($str) > 3) {
-                $allowed = !preg_match('/[^0-9A-Za-z_.-]/', $str);
+                if (preg_match('/[^0-9A-Za-z_.-]/', $str))
+                    return false;
 
                 if ($str[0] == ".")
                     return false;
 
                 if ($str[strlen($str)-1] == ".")
                     return false;
-                       
-                return $allowed;
             } else {
                 return false;
             }
+
+            return true;
         }
 
         public function check_email($email) {
@@ -127,6 +128,103 @@
                         return date('F j, Y', $date);
                 }
             }
+        }
+
+
+        public function html_cut($text, $max_length) {
+            $tags   = array();
+            $result = "";
+
+            $is_open   = false;
+            $grab_open = false;
+            $is_close  = false;
+            $in_double_quotes = false;
+            $in_single_quotes = false;
+            $tag = "";
+
+            $i = 0;
+            $stripped = 0;
+
+            $stripped_text = strip_tags($text);
+
+            while ($i < strlen($text) && $stripped < strlen($stripped_text) && $stripped < $max_length)
+            {
+                $symbol  = $text{$i};
+                $result .= $symbol;
+
+                switch ($symbol)
+                {
+                   case '<':
+                        $is_open   = true;
+                        $grab_open = true;
+                        break;
+
+                   case '"':
+                       if ($in_double_quotes)
+                           $in_double_quotes = false;
+                       else
+                           $in_double_quotes = true;
+
+                    break;
+
+                    case "'":
+                      if ($in_single_quotes)
+                          $in_single_quotes = false;
+                      else
+                          $in_single_quotes = true;
+
+                    break;
+
+                    case '/':
+                        if ($is_open && !$in_double_quotes && !$in_single_quotes)
+                        {
+                            $is_close  = true;
+                            $is_open   = false;
+                            $grab_open = false;
+                        }
+
+                        break;
+
+                    case ' ':
+                        if ($is_open)
+                            $grab_open = false;
+                        else
+                            $stripped++;
+
+                        break;
+
+                    case '>':
+                        if ($is_open)
+                        {
+                            $is_open   = false;
+                            $grab_open = false;
+                            array_push($tags, $tag);
+                            $tag = "";
+                        }
+                        else if ($is_close)
+                        {
+                            $is_close = false;
+                            array_pop($tags);
+                            $tag = "";
+                        }
+
+                        break;
+
+                    default:
+                        if ($grab_open || $is_close)
+                            $tag .= $symbol;
+
+                        if (!$is_open && !$is_close)
+                            $stripped++;
+                }
+
+                $i++;
+            }
+
+            while ($tags)
+                $result .= "</".array_pop($tags).">";
+
+            return $result;
         }
     }
 ?>
