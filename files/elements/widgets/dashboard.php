362311acf8fbd0862ac687c0174d756013bffdd0
0x6C77 <0x6C77@gmail.com>
2013-07-11 18:34:33 +0100

    Major redesign of dashboard sidebar widget!

--- files/elements/widgets/dashboard.php
+++ files/elements/widgets/dashboard.php
@@ -1,5 +1,3 @@
-                    <article class="widget dashboard">
-                        <h1><a href='/user/<?=$user->username;?>'><?=$user->username;?></a></h1>
 <?php
     if ($user->connect_msg):
 ?>
@@ -17,14 +15,24 @@
 <?php
     endif;
 ?>
-                        <section class="fluid clr">
-                            <div class='col span_7'>
-                                <img class='profile-pic' src='<?=$user->image;?>' width='100%'/>
+                    <article class="widget dashboard">
+                        <section class="fluid clr center">
+                            <h1><a href='/user/<?=$user->username;?>'><?=$user->username;?></a></h1>
+                            <div class='profile-pic'>
+                                <img src='<?=$user->image;?>'/>
+                                <a href='/settings/image' class='upload'>
+                                    <i class="icon-image"></i><br/>
+                                    Change Picture
+                                </a>
                             </div>
-                            <div class='col span_17 user-profile'>
-                                Score: <?=$user->score;?><br/>
-                                Posts: 419<br/>
-                                9 consecutive, best 163
-                            </div>
+                            <ul class='user-profile'>
+                                <li class='progress progress-score'><span style='width: 90%'>90%</span></li>
+                                <li>Score <span class='right'><?=$user->score;?></span></li>
+                                <li class='progress progress-login'><span style='width: 40%'>40%</span></li>
+                                <li>
+                                    Logins <span class='hint--bottom' data-hint="Your number of consecutive logins.&#10;Consecutive days are calculated using UTC.&#10;Your personal best is highlighted."><i class='icon-info'></i></span>
+                                    <span class='right'>10 <span>35</span></span>
+                                </li>
+                            </ul>
                         </section>
                     </article>
