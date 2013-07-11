780b29d1339294148fdbf1d88734c765bb480065
0x6C77 <0x6C77@gmail.com>
2013-07-11 18:34:58 +0100

    Removed tooltip for password restrictions

--- files/elements/widgets/register.php
+++ files/elements/widgets/register.php
@@ -14,7 +14,6 @@
                             <span class='hint--left right' data-hint="Username must be longer than 3 characters&#10;Only contain the characters; a-z A-Z _ . -"><i class='icon-info'></i></span>
                             <label for="reg_username">Username:</label>
                             <input type="text" name="reg_username" id="reg_username" autocomplete="off" value="<?=isset($_POST['reg_username'])?htmlspecialchars($_POST['reg_username']):''?>">
-                            <span class='hint--left right' data-hint="Password must be longer than 6 characters"><i class='icon-info'></i></span>
                             <label for="reg_password">Password:</label>
                             <input type="password" name="reg_password" id="reg_password" autocomplete="off">
                             <label for="reg_password_2">Repeat Password:</label>
