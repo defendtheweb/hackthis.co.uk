997ac3c97edc4db5cb27b4c1e4f3c9ececc63296
0x6C77 <0x6C77@gmail.com>
2013-08-14 15:28:49 +0100

    Latest forum posts widget

---
+++ files/elements/forum_latest.php
@@ -0,0 +1,16 @@
+<table class='forum-latest dark' style="margin-bottom: 16px; background: #101010">
+    <tbody>
+        <tr style="padding: 4px;">
+            <td><a href="/forum">Making a course on development from novice to employable?</a> <span>1 hour ago</span></td>
+            <td><a href="/user/flabbyrabbit">flabbyrabbit</a></td>
+            <td></td>
+            <td class='text-right'><span class="medal medal-green white">0</span></td>
+        </tr>
+        <tr style="padding: 4px;">
+            <td><a href="/forum">Street View turnover </a> <span>2 hours ago</span></td>
+            <td><a href="/user/flabbyrabbit">flabbyrabbit</a></td>
+            <td><a href="/user/flabbyrabbit">Osaka</a></td>
+            <td class='text-right'><span class="medal medal-green white">12</span></td>
+        </tr>
+    </tbody>
+</table>
