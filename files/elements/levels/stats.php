c8e29584a35973d8273b39f2331c752d76402671
0x6C77 <0x6C77@gmail.com>
2013-08-16 17:00:36 +0100

    Fixed incorrect details in level stats sidebar

--- files/elements/levels/stats.php
+++ files/elements/levels/stats.php
@@ -3,12 +3,38 @@
 						<table>
 							<tbody>
 <?php if (isset($level->data['author'])): ?>
-								<tr><td class="white">Author</td><td><a href='/user/<?=$level->data['author'];?>'><?=$level->data['author'];?></a></td></tr>
+								<tr>
+                                    <td class="white">Author</td>
+                                    <td><a href='/user/<?=$level->data['author'];?>'><?=$level->data['author'];?></a>
+                                    </td>
+                                </tr>
 <?php endif; ?>
-								<tr><td class="white">Reward</td><td><?=number_format($level->data['reward']);?> pts</td></tr>
-								<tr><td class="white">Completed</td><td><?=number_format($level->count);?></td></tr>
-								<tr><td class="white">Latest</td><td><time datetime="<?=date('c', strtotime($level->last_completed));?>"><?=$app->utils->timeSince($level->last_completed);?></time><br><a href="/user/<?=$level->last_user;?>"><?=$level->last_user;?></a></td>
-								<tr><td class="white">First</td><td><time datetime="<?=date('c', strtotime($level->first_completed));?>"><?=$app->utils->timeSince($level->first_completed);?></time><br><a href="/user/<?=$level->first_user;?>"><?=$level->first_user;?></a></td></tr>
+								<tr>
+                                    <td class="white">Reward</td>
+                                    <td><?=number_format($level->data['reward']);?> pts</td>
+                                </tr>
+								<tr>
+                                    <td class="white">Completed</td>
+                                    <td><?=number_format($level->count);?></td>
+                                </tr>
+<?php if (isset($level->last_completed)): ?>
+								<tr>
+                                    <td class="white">Latest</td>
+                                    <td><time datetime="<?=date('c', strtotime($level->last_completed));?>"><?=$app->utils->timeSince($level->last_completed);?></time><br>
+                                        <a href="/user/<?=$level->last_user;?>"><?=$level->last_user;?></a>
+                                    </td>
+                                </tr>
+<?php
+    endif;
+    if (isset($level->first_completed)):
+?>
+								<tr>
+                                    <td class="white">First</td>
+                                    <td><time datetime="<?=date('c', strtotime($level->first_completed));?>"><?=$app->utils->timeSince($level->first_completed);?></time><br>
+                                        <a href="/user/<?=$level->first_user;?>"><?=$level->first_user;?></a>
+                                    </td>
+                                </tr>
+<?php endif; ?>
 							</tr>
 							</tbody>
 						</table>
