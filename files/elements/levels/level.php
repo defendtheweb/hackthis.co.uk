692f4623a4b258890e329c41efb00dd9d0de90a7
0x6C77 <0x6C77@gmail.com>
2013-08-16 18:06:47 +0100

    Level forms are generated from JSON or can be included in plain html

--- files/elements/levels/level.php
+++ files/elements/levels/level.php
@@ -1,11 +1,25 @@
-<div class='level-area'>
-	<form method="POST">
-		<fieldset>
-			<label for="user">Username:</label>
-			<input type="text" autocomplete="off" id="user" name="user"><br>
-			<label for="pass">Password:</label>
-			<input type="password" autocomplete="off" id="pass" name="pass"><br>
-			<input type="submit" class="button" value="Submit">
-		</fieldset>
-	</form>
-</div>
+<?php
+    if (isset($level->data['form'])):
+?>
+    <div class='level-area'>
+<?php
+        if ($form = json_decode($level->data['form'])):
+?>
+        <form <?=isset($form->method)?'method="'.strtoupper($form->method).'"':'';?>>
+            <fieldset>
+<?php       foreach($form->fields AS $field): ?>
+                <label for="user"><?=$field->label;?>:</label>
+                <input <?=isset($field->type)?"type='{$field->type}'":'';?> autocomplete="off" <?=isset($field->name)?"id='{$field->name}' name='{$field->name}'":'';?>><br>
+<?php       endforeach; ?>
+                <input type="submit" class="button" value="Submit">
+            </fieldset>
+        </form>            
+<?php
+        else:
+            echo $level->data['form'];
+        endif;
+?>
+    </div>
+<?php
+    endif;
+?>
