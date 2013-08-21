7b90729ee5b8b1433308ecfdddfa5865ca63c7bd
0x6C77 <0x6C77@gmail.com>
2013-08-21 21:03:10 +0100

    Dynamic resizing of bbcode embedded videos

--- html/files/js/bbcode.js
+++ html/files/js/bbcode.js
@@ -6,4 +6,18 @@ $(function() {
 
 		$(this).toggleClass('active');
 	});
+
+
+
+    // Video resize
+    // Find all YouTube videos
+    var $allVideos = $(".bbcode-youtube, .bbcode-vimeo");
+    function resizeVideos() {
+        $allVideos.each(function() {
+            var $el = $(this);
+            $el.removeAttr('height').height($el.width()*0.56);
+        });
+    }
+
+    $(window).resize(resizeVideos).resize();
 });
