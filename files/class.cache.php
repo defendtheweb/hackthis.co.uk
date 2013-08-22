25b4036c0dde4fb5ed81ca9e6baed4f40382e46d
0x6C77 <0x6C77@gmail.com>
2013-08-22 17:41:57 +0100

    Created set cache function

--- files/class.cache.php
+++ files/class.cache.php
@@ -14,5 +14,12 @@
 
 			return $data;
 		}
+
+		function set($file, $content) {
+			$file = preg_replace("/[^A-Za-z0-9_-]/", '', $file);
+			$fp = fopen($this->app->config['cache'] . $file, 'w');
+			fwrite($fp, $content);
+			fclose($fp);
+		}
 	}
 ?>
