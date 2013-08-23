ba8a55945699ede70c2e8a6d7239c11aa1444095
0x6C77 <0x6C77@gmail.com>
2013-08-23 16:29:37 +0100

    Created friends tab in settings

--- files/elements/tabs_settings.php
+++ files/elements/tabs_settings.php
@@ -1,8 +1,8 @@
     <ul class='tabs'>
         <li <?=$tab=='profile'?"class='active'":'';?>><a href='/settings/'>Profile</a></li>
         <li <?=$tab=='image'?"class='active'":'';?>><a href='/settings/image.php'>Image</a></li>
-        <li <?=$tab=='friends'?"class='active'":'';?>><a href='/settings/image.php'>Friends</a></li>
-        <li <?=$tab=='security'?"class='active'":'';?>><a href='/settings/image.php'>Security</a></li>
-        <li <?=$tab=='notifications'?"class='active'":'';?>><a href='/settings/image.php'>Notifications</a></li>
+        <li <?=$tab=='friends'?"class='active'":'';?>><a href='/settings/friends.php'>Friends</a></li>
+        <li <?=$tab=='security'?"class='active'":'';?>><a href='/settings/security.php'>Security</a></li>
+        <li <?=$tab=='notifications'?"class='active'":'';?>><a href='/settings/notifications.php'>Notifications</a></li>
         <li class='right <?=$tab=='account'?'active':'';?>'><a href='/settings/account.php'>Account</a></li>
     </ul>
