226a62b703e7bfa5f0153e9b215abacb970b25af
0x6C77 <0x6C77@gmail.com>
2013-10-23 15:10:15 +0100

    Small tweak to UI css

--- files/elements/home_intro.php
+++ files/elements/home_intro.php
@@ -1,4 +1,4 @@
-<div class='home-module'>
+<div class='home-module home-module-light'>
     <a class='remove right' href='#'><i class='remove icon-remove'></i></a>
     <h1>Welcome to HackThis!!</h1>
     <p>You are awesome</p>
