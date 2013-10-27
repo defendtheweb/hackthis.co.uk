245069419dec8d80f8872cfd7b04c105c3ac0edf
0x6C77 <0x6C77@gmail.com>
2013-10-27 21:28:10 +0000

    Added t-shirt size to donation

---
+++ html/files/js/faq.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.donate input').on('keyup', function() {
+        var value = Number(this.value.replace(/[^0-9\.]+/g,""));
+
+        if (value >= 20)
+            $('.donate-perk').slideDown();
+        else
+            $('.donate-perk').slideUp();
+    });
+});
