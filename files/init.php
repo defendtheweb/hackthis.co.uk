2b888004be16003dabc9a2b82218cdcc1f186adb
0x6C77 <0x6C77@gmail.com>
2013-11-01 22:29:40 +0000

    Stopped all errors being displayed

--- files/init.php
+++ files/init.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	error_reporting(E_ALL);
-	ini_set('display_errors', '1');
+	ini_set('display_errors', '0');
 
 	// Session security flags
 	ini_set('session.cookie_httponly', 1);
