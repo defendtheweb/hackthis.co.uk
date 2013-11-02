1bb7630ec9618fcd2454af8bb8deafa5aba9d49a
0x6C77 <0x6C77@gmail.com>
2013-11-02 17:50:14 +0000

    Removed inline JS from custom scrollbars

--- html/files/js/jquery.mCustomScrollbar.js
+++ html/files/js/jquery.mCustomScrollbar.js
@@ -90,7 +90,7 @@ along with this program.  If not, see http://www.gnu.org/licenses/lgpl.html.
 				if($.support.touch){
 					mCSB_container.addClass("mCS_touch");
 				}
-				mCSB_container.after("<div class='mCSB_scrollTools' style='position:absolute;'><div class='mCSB_draggerContainer'><div class='mCSB_dragger' style='position:absolute;' oncontextmenu='return false;'><div class='mCSB_dragger_bar' style='position:relative;'></div></div><div class='mCSB_draggerRail'></div></div></div>");
+				mCSB_container.after("<div class='mCSB_scrollTools' style='position:absolute;'><div class='mCSB_draggerContainer'><div class='mCSB_dragger' style='position:absolute;'><div class='mCSB_dragger_bar' style='position:relative;'></div></div><div class='mCSB_draggerRail'></div></div></div>");
 				var mCSB_scrollTools=mCustomScrollBox.children(".mCSB_scrollTools"),
 					mCSB_draggerContainer=mCSB_scrollTools.children(".mCSB_draggerContainer"),
 					mCSB_dragger=mCSB_draggerContainer.children(".mCSB_dragger");
