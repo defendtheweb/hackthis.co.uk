cc66be3a696511ce54fdb957cb315879ad93022e
0x6C77 <0x6C77@gmail.com>
2013-11-05 06:07:23 +0000

    Subdued errors

--- files/class.cache.php
+++ files/class.cache.php
@@ -21,7 +21,7 @@
 		function set($file, $content) {
 			$file = preg_replace("/[^A-Za-z0-9_-]/", '', $file);
 			
-			if (($fp = fopen($this->app->config['cache'] . $file, 'w')) !== false) {
+			if (($fp = @fopen($this->app->config['cache'] . $file, 'w')) !== false) {
 				fwrite($fp, $content);
 				fclose($fp);
 			}
