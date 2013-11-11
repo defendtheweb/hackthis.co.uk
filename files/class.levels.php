fe542b7261cd68b59125d1f2b9f5a714bbf8bb9b
0x6C77 <0x6C77@gmail.com>
2013-11-11 20:36:40 +0000

    Added user counts and latest to level stats

--- files/class.levels.php
+++ files/class.levels.php
@@ -132,6 +132,21 @@
             // Set page details
             $this->app->page->title = ucwords($level->title);
 
+            // Get stats
+            $sql = "SELECT COUNT(user_id) AS `completed` FROM users_levels WHERE completed > 0 AND level_id = :lid";
+            $st = $this->app->db->prepare($sql);
+            $st->execute(array(':lid'=>$level->level_id));
+            $result = $st->fetch();
+            $level->count = $result->completed;
+
+            // Get latest
+            $sql = "SELECT username, completed FROM users_levels INNER JOIN users ON users.user_id = users_levels.user_id WHERE completed > 0 AND level_id = :lid ORDER BY completed DESC LIMIT 1";
+            $st = $this->app->db->prepare($sql);
+            $st->execute(array(':lid'=>$level->level_id));
+            $result = $st->fetch();
+            $level->last_completed = $result->completed;
+            $level->last_user = $result->username;
+
             return $level;
         }
 
