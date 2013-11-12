281efa20ed5cf297af332b4a779e8f72b4a7f079
0x6C77 <0x6C77@gmail.com>
2013-11-12 01:56:42 +0000

    Adding support files for admin forum page

--- html/files/js/admin_forum.js
+++ html/files/js/admin_forum.js
@@ -4,18 +4,20 @@ $(function() {
     var getCount = function(d) { return d.count };
     var getDate = function(d) { return format.parse(d.date) };
 
+    var width = $(".graph").width();
 
-    var margin = {top: 20, right: 0, bottom: 20, left: 0},
-        width = 460 - margin.left - margin.right,
-        height = 200 - margin.top - margin.bottom;
+    var margin = {top: 30, right: 20, bottom: 20, left: 30},
+        width = width - margin.left - margin.right,
+        height = 180 - margin.top - margin.bottom;
 
     var formatNumber = d3.format(".1f");
+    var  date_format = d3.time.format("%d %b");
 
     var xscale = d3.time.scale().domain(d3.extent(data, getDate)).range([0, width]);
     var yscale = d3.scale.linear().domain(d3.extent(data, getCount)).range([height,0]);
 
     var y = d3.scale.linear()
-        .domain(d3.extent(data, getCount))
+        .domain(d3.extent(data, getCount)).nice()
         .range([height, 0]);
 
     var x = d3.time.scale()
@@ -24,13 +26,14 @@ $(function() {
 
     var xAxis = d3.svg.axis()
         .scale(x)
-        .ticks(d3.time.days)
+        .ticks(d3.time.hours, 24)
+        .tickFormat(date_format)
         .orient("bottom");
 
     var yAxis = d3.svg.axis()
         .scale(y)
+        .ticks(4)
         .tickSize(width)
-        .tickFormat(formatNumbers)
         .orient("right");
 
     var svg = d3.select(".graph").append("svg")
@@ -46,14 +49,21 @@ $(function() {
 
     var gy = svg.append("g")
         .attr("class", "y axis")
+        .attr("transform", "translate(0,0)")
         .call(yAxis);
 
     gy.selectAll("g").filter(function(d) { return d; })
         .classed("minor", true);
 
     gy.selectAll("text")
-        .attr("x", 4)
-        .attr("dy", -4);
+        .attr("x", -30)
+        .attr("dy", 4);
+
+    svg.append("text")
+        .attr("text-anchor", "middle")
+        .attr("transform", "translate("+ (width/2) +",-18)")
+        .text("New forum posts over the last 7 days")
+        .classed("title", true);
 
 
     var line = d3.svg.line()
@@ -64,10 +74,4 @@ $(function() {
           .attr("d", line(data))
           .attr("stroke-width", "1")
           .attr("fill", "none");
-
-
-
-    function formatNumbers(d) {
-        return d > 999 ? (d/1000).toFixed(1) + 'K' : d
-    }
 });
