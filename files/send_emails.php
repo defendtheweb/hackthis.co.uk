4396698ca6dfb1bbd7a07fdfa609ccaac1192e6f
0x6C77 <0x6C77@gmail.com>
2013-11-25 18:14:18 +0000

    Changed email queue

--- files/send_emails.php
+++ files/send_emails.php
@@ -26,15 +26,68 @@
     do {
         $email = $app->email->getNext();
         if ($email) {
-            // build body
-            $template = file_get_contents('elements/email_template.html', true);
+            // build template
+            $template = file_get_contents('elements/emails/template.html', true);
 
-            $vars = array(
-                '{greeting}' => $email->username?"Hello " . $email->username:"Hello",
-                '{message}' => $email->body
-            );
+            // build content
+            if ($email->type == "password") {
+                $email->subject = "Password request";
+                $content = file_get_contents('elements/emails/plain_message.html', true); 
 
-            $email->body = str_replace(array_keys($vars), $vars, $template);
+                $body = "We received a request for your HackThis!! account details.<br/><br/>Username: {$email->data->username}<br/>To reset your password, click on this link: <a href='http://www.hackthis.co.uk/?request={$email->data->token}'>http://www.hackthis.co.uk/?request={$email->data->token}</a><br/><br/>If you feel you have received this message in error, delete this email. Your password can only be reset via this email.";
+
+                $vars = array(
+                    '{message}' => $body
+                );
+            } else if ($email->type == "email_confirmation") {
+                $email->subject = "Confirm your email address";
+                $content = file_get_contents('elements/emails/plain_message.html', true); 
+
+                $body = "Click on the following link to verify your e-mail address:<br/><a style='color:#ffffff; text-decoration: none;' href='https://www.hackthis.co.uk/settings/account.php?verify={$email->data->token}'>https://www.hackthis.co.uk/settings/account.php?verify={$email->data->token}</a>";
+
+                if ($email->data->new) {
+                    $body = "Thank you for signing up for a <a style='color:#ffffff; text-decoration: none;' href='https://www.hackthis.co.uk/'>HackThis!!</a> account.<br/><br/>" . $body;
+                }
+
+                $vars = array(
+                    '{message}' => $body
+                );
+            } else if ($email->type == "ticket_reply") {
+                $email->subject = "Ticket reply";
+                $content = file_get_contents('elements/emails/plain_message.html', true); 
+
+                $body = "A reply has been added to a ticket you opened. To view the message please click the following link:<br/><a style='color:#ffffff; text-decoration: none;' href='https://www.hackthis.co.uk/contact?view={$email->data->id}";
+
+                if ($email->data->email)
+                    $body .= "&email={$email->data->email}";
+
+                $body .= "'>https://www.hackthis.co.uk/contact?view={$email->data->id}";
+
+                if ($email->data->email)
+                    $body .= "&email={$email->data->email}";
+
+                $body .= "</a>";
+
+                $vars = array(
+                    '{message}' => $body
+                );
+            } else if ($email->type == "friend") {
+                $email->subject = "Friend request";
+                $content = file_get_contents('elements/emails/friend_request.html', true); 
+
+                $vars = array(
+                    '{username}' => $email->data->username,
+                    '{score}' => $email->data->score,
+                    '{posts}' => $email->data->posts,
+                    '{image}' => $email->data->image
+                );
+            } else {
+                return false;
+            }
+            
+            $content = str_replace(array_keys($vars), $vars, $content);
+
+            $email->body = str_replace('{content}', $content, $template);
 
             $app->email->send($email);
         }
