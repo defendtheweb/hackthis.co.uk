48f5a5cf268ff9c7552903e6b1faf8b67d89441d
0x6C77 <0x6C77@gmail.com>
2013-11-28 17:17:07 +0000

    Added IDL code

--- html/files/js/main.js
+++ html/files/js/main.js
@@ -4,6 +4,17 @@ while( $('.dashboard h1 a').width() > $('.dashboard').width() ) {
 }
 
 
+// Internet defense league
+window._idl = {};
+_idl.variant = "banner";
+(function() {
+    var idl = document.createElement('script');
+    idl.type = 'text/javascript';
+    idl.async = true;
+    idl.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'members.internetdefenseleague.org/include/?url=' + (_idl.url || '') + '&campaign=' + (_idl.campaign || '') + '&variant=' + (_idl.variant || 'banner');
+    document.getElementsByTagName('body')[0].appendChild(idl);
+})();
+
 
 var loggedIn = true; // overwritten in guest.js
 
