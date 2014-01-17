6b38538313ec84ff5f85faa24ae772e65a0f4bad
0x6C77 <0x6C77@gmail.com>
2014-01-17 21:32:03 +0000

    Fixed wide ads

--- files/elements/widgets/adverts.php
+++ files/elements/widgets/adverts.php
@@ -3,6 +3,22 @@
         if (!$app->user->loggedIn && defined('LANDING_PAGE') && LANDING_PAGE):
 ?>
                     <article class="widget adverts">
+                        <div class="center">
+                            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                            <!-- landing page -->
+                            <ins class="adsbygoogle"
+                                 style="display:inline-block;width:336px;height:280px"
+                                 data-ad-client="ca-pub-1120564121036240"
+                                 data-ad-slot="7541400418"></ins>
+                            <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                    </article>
+<?php
+        else:
+?>
+                    <article class="widget adverts">
                         <h1>Adverts</h1>
                         <div class="center">
                             <script type="text/javascript"><!--
@@ -14,22 +30,6 @@
                                 //-->
                             </script>
                             <script type="text/javascript" src="https://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
-                        </div>
-                    </article>
-<?php
-        else:
-?>
-                    <article class="widget adverts">
-                        <div class="center">
-                            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                            <!-- landing page -->
-                            <ins class="adsbygoogle"
-                                 style="display:inline-block;width:336px;height:280px"
-                                 data-ad-client="ca-pub-1120564121036240"
-                                 data-ad-slot="7541400418"></ins>
-                            <script>
-                            (adsbygoogle = window.adsbygoogle || []).push({});
-                            </script>
                         </div>
                     </article>
 <?php
