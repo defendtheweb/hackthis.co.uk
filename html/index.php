0b0a42b7560255d8e0451cb42a552db148a8c278
0x6C77 <0x6C77@gmail.com>
2014-03-18 21:00:50 +0000

    removed google ads from fancy homepage

--- html/index.php
+++ html/index.php
@@ -37,18 +37,6 @@
             $app->utils->message('Your account has been successfully deleted. Painful though parting be, I bow to you as I see you off to distant clouds. ', 'info');
         }
 ?>
-                <div class='center row'>
-                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- landing_header -->
-                    <ins class="adsbygoogle"
-                         style="display:inline-block;width:728px;height:90px"
-                         data-ad-client="ca-pub-1120564121036240"
-                         data-ad-slot="9555223615"></ins>
-                    <script>
-                    (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                </div>
-
                 <div class='row landing_blurb'>
                     <h1>Want to learn about hacking and network security? Discover how hacks, dumps and defacements are performed and secure your website against hackers with HackThis!!</h1>
                 </div>
