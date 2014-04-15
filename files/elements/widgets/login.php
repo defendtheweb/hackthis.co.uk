0be4048594d540a22a9078161e3890638684b386
Doron Linder <doron@vim-adventures.com>
2014-04-15 23:04:21 +0300

    Autofocus on username only for returning users

--- files/elements/widgets/login.php
+++ files/elements/widgets/login.php
@@ -10,9 +10,24 @@
 <?php
     endif;
 ?>
+                        <script>
+                            // We would like to set focus to the username element only if this is a
+                            // returning user, meaning that he already logged in in the past.
+                            // If he's not, on smaller screens the screen will scroll down and new
+                            // users won't see the intro text.
+                            // Returning users are marked by the key ht_returning_user in local
+                            // stroage upon login.
+
+                            if (Modernizr.localstorage && window.localStorage['ht_returning_user'] === 'true') {
+                                $( document).ready(function() {
+                                    $("#username").focus();
+                                });
+                            }
+                        </script>
+
                         <form id="login_form" action="?login" method="POST">
                             <label for="username">Username:</label>
-                            <input type="text" name="username" id="username" autofocus>
+                            <input type="text" name="username" id="username">
                             <label for="password">Password:</label>
                             <input type="password" name="password" id="password">
                             <a class="white" href="/?request">Forgot details?</a>
