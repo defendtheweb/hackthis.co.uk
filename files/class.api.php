a931fc5918cbbae410d4ec58351978b32942d7c8
Flabby <flabbyrabbit@debian>
2015-02-20 20:16:09 +0000

    Updated API access

--- files/class.api.php
+++ files/class.api.php
@@ -1,6 +1,9 @@
 <?php
     class api {
 
+        //=====================================================
+        // CONSTRUCTOR
+        //=====================================================
         public function __construct($app, $key) {
             header('Cache-Control: no-cache, must-revalidate');
             header('Expires: Mon, 26 Jul 1997 00:00:00 GMT');
@@ -14,7 +17,7 @@
              * When no API key is present, use users privileges
              */
             if (!isset($this->privileges) && $this->app->user->loggedIn) {
-                $this->privileges = "inherit";
+                $this->privileges = array('user.profile');
             }
 
             if (!isset($this->privileges)) {
@@ -27,14 +30,82 @@
                 $this->respond(400);
             }
 
+            // Check privileges
+            $this->hasPrivilege($method);
+
             switch ($method) {
+                case 'irc.log': $this->logIrc(); break;
+                case 'user.login': $this->user('login'); break;
                 case 'user.profile': $this->user('profile'); break;
             }
 
             $this->respond(400);
         }
 
-        public function respond($status, $data=null) {
+
+        //=====================================================
+        // REQUEST HANDLERS
+        //=====================================================
+
+        //-----------------------------------------------------
+        // User
+        //-----------------------------------------------------
+        private function user($request) {
+            $response = new stdClass();
+
+            switch ($request) {
+                case 'profile': $response->profile = $this->userProfile(); break;
+                case 'login': $response = $this->userLogin(); break;
+            }
+
+            $this->respond(200, $response);
+        }
+
+        private function userProfile() {
+            $profile = new profile($_GET['user'], true); ;
+
+            unset($profile->email);
+
+            return $profile;
+        }
+
+        private function userLogin() {
+            // No idea yet
+        }
+
+
+        //-----------------------------------------------------
+        // IRC
+        //-----------------------------------------------------
+        private function logIrc() {
+            if (!isset($_POST['nick']) || !isset($_POST['chan']) || !isset($_POST['msg']))
+                throw new Exception('Missing data fields');
+
+            $_POST['msg'] = preg_replace('/\x01/', '', $_POST['msg']);
+
+            $st = $this->app->db->prepare('INSERT INTO irc_logs (`nick`, `channel`, `log`)
+                    VALUES (:nick, :chan, :msg)');
+            $result = $st->execute(array(':nick' => $_POST['nick'], ':chan' => $_POST['chan'], ':msg' => $_POST['msg']));
+
+
+            // Calculate stats
+            $st = $this->app->db->prepare('INSERT INTO irc_stats (`nick`, `lines`, `words`, `chars`)
+                    VALUES (:nick, :lines, :words, :chars)
+                    ON DUPLICATE KEY UPDATE `lines`=`lines`+:lines, `words`=`words`+:words, `chars`=`chars`+:chars, `time`=NOW()');
+
+            $st->bindValue(':nick', $_POST['nick'], PDO::PARAM_INT);
+            $st->bindValue(':lines', 1, PDO::PARAM_INT);
+            $st->bindValue(':words', str_word_count($_POST['msg']), PDO::PARAM_INT);
+            $st->bindValue(':chars', strlen($_POST['msg']), PDO::PARAM_INT);
+            $result = $st->execute();
+
+            $this->respond(200);
+        }
+
+        //=====================================================
+        // HELPER FUNCTIONS
+        //=====================================================
+        private function respond($status, $data=null) {
             if (!$data) {
                 $data = new stdClass();
             }
@@ -66,6 +137,11 @@
         }
 
         private function checkKey($key) {
+            // if ($this->app->config('api') == $key) {
+            //     $this->privileges = array('irc.*', 'user.profile');
+            //     return true;
+            // }
+
             $st = $this->app->db->prepare('SELECT privileges FROM api_clients WHERE `key` = :key LIMIT 1');
             $st->execute(array(':key' => $key));
             $result = $st->fetch();
@@ -78,54 +154,20 @@
         }
 
 
-        private function user($request) {
-            $response = new stdClass();
+        private function hasPrivilege($privilege) {
+            // get subject
+            $subject = strtok($privilege, '.');
 
-            if ($request == 'profile') {
-                $response->profile = new profile($_GET['user'], true);
+            if ($subject = 'public') {
+                return true;
             }
 
-            $this->respond(200, $response);
-        }
+            $globalPrivilege = $subject . '.*';
 
-
-
-
-
-        public function process() {
-            if (!isset($_GET['action']))
-                throw new Exception('Invalid request');
-
-            switch ($_GET['action']) {
-                case 'irc.log': $this->logIrc(); break;
-                default: throw new Exception('Invalid request');
+            if (!in_array($privilege, $this->privileges) &&
+                !in_array($globalPrivilege, $this->privileges)) {
+                $this->respond(403);
             }
-        }
-
-
-
-        /* IRC */
-        public function logIrc() {
-            if (!isset($_POST['nick']) || !isset($_POST['chan']) || !isset($_POST['msg']))
-                throw new Exception('Missing data fields');
-
-            $_POST['msg'] = preg_replace('/\x01/', '', $_POST['msg']);
-
-            $st = $this->app->db->prepare('INSERT INTO irc_logs (`nick`, `channel`, `log`)
-                    VALUES (:nick, :chan, :msg)');
-            $result = $st->execute(array(':nick' => $_POST['nick'], ':chan' => $_POST['chan'], ':msg' => $_POST['msg']));
-
-
-            // Calculate stats
-            $st = $this->app->db->prepare('INSERT INTO irc_stats (`nick`, `lines`, `words`, `chars`)
-                    VALUES (:nick, :lines, :words, :chars)
-                    ON DUPLICATE KEY UPDATE `lines`=`lines`+:lines, `words`=`words`+:words, `chars`=`chars`+:chars, `time`=NOW()');
-
-            $st->bindValue(':nick', $_POST['nick'], PDO::PARAM_INT);
-            $st->bindValue(':lines', 1, PDO::PARAM_INT);
-            $st->bindValue(':words', str_word_count($_POST['msg']), PDO::PARAM_INT);
-            $st->bindValue(':chars', strlen($_POST['msg']), PDO::PARAM_INT);
-            $result = $st->execute();
         }
 
     }
