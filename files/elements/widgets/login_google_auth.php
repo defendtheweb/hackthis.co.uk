3ab3152c528998ecedc28c25741401ef41042988
Luke <luke.ward@sequelgroup.co.uk>
2015-06-12 16:22:01 +0100

    Tweaked wording on enter 2FA code login box

--- files/elements/widgets/login_google_auth.php
+++ files/elements/widgets/login_google_auth.php
@@ -1,11 +1,6 @@
                     <article class="widget widget-login">
-                        <!-- <h1>Login</h1> -->
-                        <div class='msg'>
-                            <i class='icon-error'></i>
-                            Enter Google Authenticator Code
-                        </div>
                         <form id="g_auth_form" action="?g_auth" method="POST">
-                            <label for="username">Code:</label>
+                            <label for="username">Google Authenticator Code:</label>
                             <input type="password" name="g_code" id="password">
                             <input type="submit" value="Submit" class="button">
                         </form>
