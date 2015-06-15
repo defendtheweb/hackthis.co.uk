2429ff0cefbaa1620b7261c604b763289fed8abd
flabbyrabbit <flabbyrabbit@hackthis.co.uk>
2015-06-15 09:12:42 +0000

    Changed 2FA field to number for easier input on mobile devices

--- files/elements/widgets/login_google_auth.php
+++ files/elements/widgets/login_google_auth.php
@@ -1,7 +1,7 @@
                     <article class="widget widget-login">
                         <form id="g_auth_form" action="?g_auth" method="POST">
-                            <label for="username">Google Authenticator Code:</label>
-                            <input type="password" name="g_code" id="password">
+                            <label for="password">Google Authenticator Code:</label>
+                            <input type="number" name="g_code" id="password">
                             <input type="submit" value="Submit" class="button">
                         </form>
                     </article>
