c2cd7451702caf831580415ba3726998c80ae325
ThibmoRozier <thibmorozier@gmail.com>
2015-12-17 14:57:46 +0100

    Fix(install): Fixing 500 internal server error (Apache2 on Ubuntu)   *  Fixes: Invalid command 'RewriteEngine' #167

--- install_hackthis_ubuntu.sh
+++ install_hackthis_ubuntu.sh
@@ -177,7 +177,6 @@ mysql -u $mysql_user $pass_clause < setup.sql
 echo -e "\t HackThis database was initialized"
 
 # Setting up directories and permissions
-
 echo Setting up directories and permissions
 mkdir -p html/files/css/min{,/light,/dark}
 chmod 777 html/files/css/min{,/light,/dark}
@@ -189,5 +188,10 @@ mkdir -p files/cache{,/twig}
 chmod 777 files/cache{,/twig}
 mkdir -p files/logs
 chmod 777 files/logs
+
+# Enabeling ModRewrite to solve RewriteEngine issue
+echo Enabeling Mod_Rewrite
+a2enmod rewrite
+service apache2 restart
 
 echo Done!
